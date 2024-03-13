a1ce4a34aba67048b585e9da57eef10a75d3efbc
Kwailly <kylllian.wailly@gmail.com>
2024-03-13 17:37:52 +0100

    17h40

--- es.h
+++ es.h
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "livre.h"
+
 
 
 char *lire(char *lachaine,int nbMAXcaracAsaisir);
 char *lireChaine(const char *, char * , int );
 void afficherChaine(const char *,const char *);
-void lireDateSysteme(T_Emp *E);
+
 #endif
